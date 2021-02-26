6268b1b159978244dbed112b0bb002a94585650f
hongchii <flyh0321@gmail.com>
2021-02-26 20:07:32 +0900

    자바 배열 - 버블정렬 알고리즘으로 정렬하기

---
+++ src/Ch5/ArrayEx10.java
@@ -0,0 +1,36 @@
+package Ch5;
+
+public class ArrayEx10 {
+
+	public static void main(String[] args) {
+		int[] numArr = new int[10];
+
+		for (int i = 0; i < numArr.length; i++) {
+			System.out.print(numArr[i] = (int) (Math.random() * 10));
+		}
+		System.out.println();
+
+		for (int i = 0; i < numArr.length - 1; i++) {
+			boolean changed = false; // 자리바꿈이 발생했는지 체크
+
+			for (int j = 0; j < numArr.length - 1 - i; j++) {
+				if (numArr[j] > numArr[j + 1]) { // 왼쪽의 값이 작으면 서로 바꿈
+					int temp = numArr[j];
+					numArr[j] = numArr[j+1];
+					numArr[j + 1] = temp;
+					changed = true; // 자리 바꿈이 발생했으니 true
+					
+				}
+			}
+			if (!changed)
+				break; // 자리바꿈이 없으면 반복문을 벗어남
+
+			for (int k = 0; k < numArr.length; k++) { 
+				System.out.print(numArr[k]);
+			}
+			System.out.println();
+
+		}
+	}
+
+}
