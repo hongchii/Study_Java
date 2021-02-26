42fa4fb688b64910efb2d6d22530c8cc7abe3060
hongchii <flyh0321@gmail.com>
2021-02-26 12:02:19 +0900

    배열 섞기, 로또 번호 생성

---
+++ src/Ch5/ArrayEx7.java
@@ -0,0 +1,24 @@
+package Ch5;
+
+public class ArrayEx7 {
+
+	public static void main(String[] args) {
+		int[] numArr = new int[10];
+
+		for (int i = 0; i < numArr.length; i++) {
+			numArr[i] = i;
+			System.out.print(numArr[i]);
+		}
+		System.out.println();
+
+		for (int i = 0; i < 100; i++) {
+			int n = (int) (Math.random() * 10); // 0~9 중 임의의 숫자를 얻는다
+			int tmp = numArr[0];
+			numArr[0] = numArr[n];
+			numArr[n] = tmp;
+		}
+		for (int i = 0; i < numArr.length; i++)
+			System.out.print(numArr[i]);
+	}
+
+}
