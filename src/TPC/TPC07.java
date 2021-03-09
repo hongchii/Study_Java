b65ee92ceb3254e726c7ec1bf6c8868844105d1c
hongchii <flyh0321@gmail.com>
2021-03-09 12:43:32 +0900

    인프런 Java TPC6 메서드는 변수다

---
+++ src/TPC/TPC07.java
@@ -0,0 +1,30 @@
+package TPC;
+
+public class TPC07 {
+
+	public static void main(String[] args) {
+		int a = 20;
+		float b = 56.7f;
+		float v = sum(a, b); // Call By Value(값)
+		System.out.println(v);
+
+		int[] arr = { 1, 2, 3, 4, 5 };
+		// 배열의 총합.
+		int vv = arrSum(arr); // Call By Reference(번지)
+		System.out.println(vv);
+	}
+
+	private static int arrSum(int[] x) {
+		int hap = 0;
+		for (int i = 0; i < x.length; i++) {
+			hap += x[i];
+		}
+		return hap;
+	}
+
+	public static float sum(int a, float b) {
+		float v = a + b;
+		return v;
+	}
+
+}
