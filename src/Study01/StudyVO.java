a0e2a785f625721bdfd1f104f062130e3a480fbc
hongchii <flyh0321@gmail.com>
2021-05-31 23:49:40 +0900

    스터디관리 코드 수정

--- src/Study01/StudyVO.java
+++ src/Study01/StudyVO.java
@@ -1,12 +1,13 @@
 package Study01;
 
 public class StudyVO {
-	 String name;
-	 int deposit;
-	 char ch;
-	 char task;
-	 char chk1;
-	 char chk2;
-	 
+	 String name; // 멤버이름
+	 int deposit; // 보증금
+	 int fine; // 벌금
+	 int balance; // 잔액
+	 char ch; // 챕터 수행여부
+	 char task; // 과제 수행여부 
+	 char chk1; // 중간점검 참석여부
+	 char chk2; // 마지막점검 참석여부
 	 
 }
