12756c7a47d5f869df2e20a4ea823d8c0206afe4
hongchii <flyh0321@gmail.com>
2021-06-30 23:09:42 +0900

    카페 주문 콘솔 출력 프로그램

---
+++ src/CafeOrderProgram/OrderMain.java
@@ -0,0 +1,25 @@
+package CafeOrderProgram;
+
+import java.util.Scanner;
+
+public class OrderMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("메뉴");
+		System.out.println("1.아메리카노 1000원");
+		System.out.println("2.바닐라라떼 2500원");
+		System.out.println("3.레몬에이드 3000원");
+		System.out.println("4.자몽에이드 3000원");
+		System.out.println("5.수박주스  4000원");
+		System.out.println("6.토마토주스 4000원");
+		System.out.println("----------------------------");
+		System.out.println();
+		System.out.println("음료를 선택해주세요 : ");
+		
+		
+	}
+
+}
