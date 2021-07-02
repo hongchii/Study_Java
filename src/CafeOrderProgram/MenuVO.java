0c6c4ba4abc978bbda1909746201c57bf2d1794f
hongchii <flyh0321@gmail.com>
2021-07-02 22:04:34 +0900

    카페 주문 콘솔 출력 프로그램 구현중

--- src/CafeOrderProgram/MenuVO.java
+++ src/CafeOrderProgram/MenuVO.java
@@ -1,5 +1,42 @@
 package CafeOrderProgram;
 
 public class MenuVO {
+	private String coffee;
+	private String iceOrHot;
+	private String shot;
+	private String size;
+	private String takeoutYn;
+	
+	public String getCoffee() {
+		return coffee;
+	}
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+	public String getIceOrHot() {
+		return iceOrHot;
+	}
+	public void setIceOrHot(String iceOrHot) {
+		this.iceOrHot = iceOrHot;
+	}
+	public String getShot() {
+		return shot;
+	}
+	public void setShot(String shot) {
+		this.shot = shot;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getTakeoutYn() {
+		return takeoutYn;
+	}
+	public void setTakeoutYn(String takeoutYn) {
+		this.takeoutYn = takeoutYn;
+	}
+	
 	
 }
--- src/CafeOrderProgram/OrderMain.java
+++ src/CafeOrderProgram/OrderMain.java
@@ -1,13 +1,50 @@
 package CafeOrderProgram;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OrderMain {
 
 	public static void main(String[] args) {
+        
+		ArrayList list = new ArrayList();
 		
-		Scanner scan = new Scanner(System.in);
+		int menu;
 		
+		while(true) {
+			
+			Scanner scan = new Scanner(System.in);
+			menu = scan.nextInt();
+			System.out.println();
+			
+			switch(menu) {
+			case 1 :
+				setOrder(list);
+				break;
+			case 2 :
+				
+				break;
+			case 3 :
+				
+				break;
+			case 4 :
+				
+				break;
+			case 5 :
+				
+				break;
+			case 6 :
+				
+				break;
+			default:
+				
+			}
+			
+		}
+		
+	} // main end
+	
+	static void menu() {
 		System.out.println("메뉴");
 		System.out.println("1.아메리카노 1000원");
 		System.out.println("2.바닐라라떼 2500원");
@@ -19,7 +56,9 @@ public class OrderMain {
 		System.out.println();
 		System.out.println("음료를 선택해주세요 : ");
 		
+	}
+	
+	static void setOrder(ArrayList list) {
 		
 	}
-
 }
