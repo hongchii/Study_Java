4e6f4bf53db18601fc4eeeac9b89ffe50ca5ab46
hongchii <flyh0321@gmail.com>
2021-07-15 20:41:52 +0900

    add : Lambda ex04

---
+++ src/Ch14/LambdaEx4.java
@@ -0,0 +1,35 @@
+package Ch14;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LambdaEx4 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < 10; i++)
+			list.add(i);
+
+		// list의 모든 요소를 출력
+		list.forEach(i -> System.out.println(i + ","));
+		System.out.println();
+
+		// list에서 2 또는 3의 배수를 제거한다.
+		list.removeIf(x -> x % 2 == 0 || x % 3 == 0);
+		System.out.println(list);
+
+		list.replaceAll(i -> i * 10);
+		System.out.println(list);
+
+		Map<String, String> map = new HashMap<>();
+		map.put("1", "1");
+		map.put("2", "2");
+		map.put("3", "3");
+		map.put("4", "4");
+
+		map.forEach((k, v) -> System.out.print("{" + k + "," + v + "},"));
+		System.out.println();
+	}
+
+}
